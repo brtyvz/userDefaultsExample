b03f04eab603bec583b054fc9d02ae3560febfc8
brtyvz <beratttyavuzz@gmail.com>
2022-03-09 10:39:27 +0300

    Initial Commit

---
+++ seguedeneme/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  seguedeneme
+//
+//  Created by Berat Yavuz on 9.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
